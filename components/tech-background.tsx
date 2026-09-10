fb85c141bb757cc6e90e656c4509d6278bed7b26
mirai-333 <miraitsuda3@gmail.com>
2026-09-10 14:46:10 +0900

    Refresh animated technology background

--- components/tech-background.tsx
+++ components/tech-background.tsx
@@ -4,10 +4,16 @@ export function TechBackground() {
       aria-hidden="true"
       className="pointer-events-none fixed inset-0 -z-10 overflow-hidden"
     >
-      {/* slow drifting technical grid */}
       <div className="tech-grid absolute inset-0" />
-      {/* soft accent glow that breathes */}
-      <div className="tech-glow absolute left-1/2 top-0 h-[40rem] w-[40rem] -translate-x-1/2 -translate-y-1/3 rounded-full" />
+      <div className="tech-orb tech-orb-a absolute -left-40 -top-40 h-[34rem] w-[34rem] rounded-full" />
+      <div className="tech-orb tech-orb-b absolute -right-44 top-[18%] h-[30rem] w-[30rem] rounded-full" />
+      <div className="tech-orb tech-orb-c absolute bottom-[-15rem] left-[28%] h-[38rem] w-[38rem] rounded-full" />
+      <div className="tech-haze absolute inset-0" />
+      <div className="floating-dot floating-dot-1" />
+      <div className="floating-dot floating-dot-2" />
+      <div className="floating-dot floating-dot-3" />
+      <div className="floating-dot floating-dot-4" />
+      <div className="floating-dot floating-dot-5" />
     </div>
   )
 }
