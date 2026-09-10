3bc91e04bee0f145c73e5153b20d2dfadce60648
mirai-333 <miraitsuda3@gmail.com>
2026-09-10 14:48:34 +0900

    Fix animated background stacking layer

--- components/tech-background.tsx
+++ components/tech-background.tsx
@@ -2,18 +2,20 @@ export function TechBackground() {
   return (
     <div
       aria-hidden="true"
-      className="pointer-events-none fixed inset-0 -z-10 overflow-hidden"
+      className="pointer-events-none fixed inset-0 z-0 overflow-hidden"
     >
-      <div className="tech-grid absolute inset-0" />
-      <div className="tech-orb tech-orb-a absolute -left-40 -top-40 h-[34rem] w-[34rem] rounded-full" />
-      <div className="tech-orb tech-orb-b absolute -right-44 top-[18%] h-[30rem] w-[30rem] rounded-full" />
-      <div className="tech-orb tech-orb-c absolute bottom-[-15rem] left-[28%] h-[38rem] w-[38rem] rounded-full" />
       <div className="tech-haze absolute inset-0" />
-      <div className="floating-dot floating-dot-1" />
-      <div className="floating-dot floating-dot-2" />
-      <div className="floating-dot floating-dot-3" />
-      <div className="floating-dot floating-dot-4" />
-      <div className="floating-dot floating-dot-5" />
+      <div className="tech-grid absolute inset-0" />
+
+      <div className="tech-orb tech-orb-a absolute -left-32 top-16 h-[32rem] w-[32rem] rounded-full" />
+      <div className="tech-orb tech-orb-b absolute -right-40 top-[28%] h-[36rem] w-[36rem] rounded-full" />
+      <div className="tech-orb tech-orb-c absolute left-[38%] top-[62%] h-[30rem] w-[30rem] rounded-full" />
+
+      <span className="floating-dot floating-dot-1" />
+      <span className="floating-dot floating-dot-2" />
+      <span className="floating-dot floating-dot-3" />
+      <span className="floating-dot floating-dot-4" />
+      <span className="floating-dot floating-dot-5" />
     </div>
   )
 }
